9c4e2920536ac36b6e4f9b74a5b5f9dfc94278a1
jailton-costa <jailton_pereira@estudante.sesisenai.org.br>
2024-08-23 17:21:22 -0300

    adisonar primero progeto

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>oi tudo bem tudo certo 😀👍 e os guri 👍😎👍</h1>
+      
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam velit, inventore magnam obcaecati molestiae perspiciatis. Repellat eligendi repellendus, accusamus incidunt animi quos sapiente est repudiandae possimus dolorum rerum, minima aut.
+
+    </>
+  )
+}
+
+export default App
